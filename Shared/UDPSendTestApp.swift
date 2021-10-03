bfbe30b1f95015afa9d0a3c458453ccb582aa830
Markus Fritze <mfritze@apple.com>
2021-10-03 23:56:44 +0200

    Initial Commit

---
+++ Shared/UDPSendTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDPSendTestApp.swift
+//  Shared
+//
+//  Created by Markus Fritze on 03.10.21.
+//
+
+import SwiftUI
+
+@main
+struct UDPSendTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
